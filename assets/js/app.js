1db3263de6b8dd3d7c30fcf5f3be1834de83a39b
Sheikh Rahmatulla Sakib <kishorsakib10@gmail.com>
2023-02-24 11:30:56 +0600

    updated

--- assets/js/app.js
+++ assets/js/app.js
@@ -118,7 +118,7 @@ let footer = $(`
       <!--SVG code for quote-left icon-->
       <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" focusable="false" width="1.5em" height="1.5em" style="-ms-transform: rotate(360deg); -webkit-transform: rotate(360deg); transform: rotate(360deg);" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path d="M7 21a4 4 0 0 1-4-4c0-1.473 1.333-6.14 4-14h2L7 13a4 4 0 1 1 0 8zm10 0a4 4 0 0 1-4-4c0-1.473 1.333-6.14 4-14h2l-2 10a4 4 0 1 1 0 8z" fill="#fff" fill-rule="evenodd"/></svg>
       </div>
-      <p class="tag" >I'm Highly motivated and enthusiastic Data Scientist worked at various positions to gather knowledge from different sectors to have a bigger picture and a broader sense. I have enriched knowledge in Deep Learning and traditional Machine Learning. Research Interest Semantic Analysis | Disease Detection | Classification | Computer Vision | NLP | Data Visualization | GAN specialties: Classification using CNN | Pretrained Model | Data Preprocessing</p>
+      <p class="tag" >As Software Engineers,We don't just write code.We craft elegant solutions that solve real-world problems and empower people to do more with technology.</p>
     </div>
 
     
